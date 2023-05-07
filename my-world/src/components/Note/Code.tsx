3d9692c4f0ad4a7fa71a43b8fd1bf25737546cb3
1g2g <wonji126326@gmail.com>
2023-05-07 22:19:14 +0900

    Fix : #8 코드 블럭 라이브러리 적용

--- my-world/src/components/Note/Code.tsx
+++ my-world/src/components/Note/Code.tsx
@@ -1,13 +1,17 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState } from "react";
 import { RootState } from "modules";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { storeCode } from "modules/NoteReducer";
-import { keywordsMap } from "assets/SyntaxKeywords";
+import Highlight from "react-highlight";
+import "highlight.js/styles/github.css";
+
+const LANGUAGE = ["javascript", "java", "python", "c", "c++", "kotlin"];
 
 export const Code = () => {
   const [modifyCode, setModifyCode] = useState(false);
-  const [code, setCode] = useState<string[]>([]);
+  const [lang, setLang] = useState("");
+
   const inputRef = useRef<HTMLTextAreaElement>(null);
   const contentsNow = useSelector((state: RootState) => state.NoteReducer);
   const dispatch = useDispatch();
@@ -18,55 +22,28 @@ export const Code = () => {
   const saveNote = () => {
     if (inputRef.current?.value) {
       store(inputRef.current.value);
-      splitCode(inputRef.current.value);
     }
     setModifyCode(false);
   };
 
-  const splitCode = (codes: string) => {
-    let i = 0;
-    let prevIdx = 0;
-    const newCode: string[] = [];
-
-    for (let c of codes) {
-      const mat = c.match(/[.\s(){};<>[\]]/);
-      if (mat !== null) {
-        newCode.push(codes.slice(prevIdx, i));
-        newCode.push(codes.slice(i, i + 1));
-
-        prevIdx = i + 1;
-      }
-      i++;
-    }
-    newCode.push(codes.slice(prevIdx));
-    setCode(newCode);
-  };
-  useEffect(() => {
-    splitCode(contentsNow.code);
-  }, []);
-  const ColoredCode = () => {
-    return (
-      <>
-        {code.map((c, idx) => {
-          if (c.indexOf("use") !== -1 || c.indexOf("set") !== -1)
-            return (
-              <span key={idx} style={{ color: "rgb(115 131 243)" }}>
-                {c}
-              </span>
-            );
-          return (
-            <span key={idx} style={{ color: keywordsMap.get(c.toUpperCase()) }}>
-              {c}
-            </span>
-          );
-        })}
-      </>
-    );
-  };
   return (
     <>
       {modifyCode ? (
         <>
+          <select
+            defaultValue={lang}
+            onChange={(e) => {
+              setLang(e.currentTarget.value);
+            }}
+          >
+            {LANGUAGE.map((p) => {
+              return (
+                <option value={p} key={p}>
+                  {p}
+                </option>
+              );
+            })}
+          </select>
           <textarea
             ref={inputRef}
             maxLength={5000}
@@ -76,7 +53,7 @@ export const Code = () => {
         </>
       ) : (
         <>
-          <ColoredCode />
+          <Highlight className={lang}>{contentsNow.code}</Highlight>
           <button onClick={() => setModifyCode(true)}>수정</button>
         </>
       )}
