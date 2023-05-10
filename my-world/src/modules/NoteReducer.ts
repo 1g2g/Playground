716eb4dd22e136b186ed7b2b544d00bf7d4f87e1
1g2g <wonji126326@gmail.com>
2023-05-10 15:35:27 +0900

    Fix : #8 notepad 자동저장

--- my-world/src/modules/NoteReducer.ts
+++ my-world/src/modules/NoteReducer.ts
@@ -1,35 +1,20 @@
 export type Note = {
-  code: string;
   notepad: string;
 };
 const NOTE_PAD = "add text to notepad store" as const;
-const CODE_BLOCK = "add codeblock to codeblock store" as const;
 export const storeNotepad = (text: string) => ({
   type: NOTE_PAD,
   payload: text,
 });
-export const storeCode = (text: string) => ({
-  type: CODE_BLOCK,
-  payload: text,
-});
-type NoteAction =
-  | ReturnType<typeof storeNotepad>
-  | ReturnType<typeof storeCode>;
+type NoteAction = ReturnType<typeof storeNotepad>;
 const initialState: Note = {
-  code: "",
   notepad: "",
 };
 export function NoteReducer(state: Note = initialState, action: NoteAction) {
   switch (action.type) {
     case NOTE_PAD:
       return {
-        code: state.code,
         notepad: action.payload,
-      };
-    case CODE_BLOCK:
-      return {
-        code: action.payload,
-        notepad: state.notepad,
       };
     default:
       return state;
