6727cff0867f87d1a89119f58ec0da30c6b2c0ce
1g2g <wonji126326@gmail.com>
2023-05-15 19:22:08 +0900

    Feat : #10 닉네임 변경

--- my-world/src/components/Welcome/Settings.tsx
+++ my-world/src/components/Welcome/Settings.tsx
@@ -1,18 +1,38 @@
+import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "modules";
 import { setColor } from "modules/SettingReducer";
+import { NickNameInput } from "./NicknameInput";
 import "components/Welcome/welcome.scss";
 export const Settings = () => {
-  const { color } = useSelector((state: RootState) => state.SettingReducer);
+  const [possibleToInput, setPossibleToInput] = useState(false);
+  const { color, nickname } = useSelector(
+    (state: RootState) => state.SettingReducer
+  );
   const dispatch = useDispatch();
   const onChangeColor = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(setColor(e.currentTarget.value));
   };
+  const changeNickname = () => {
+    setPossibleToInput(true);
+  };
+  useEffect(() => {
+    setPossibleToInput(false);
+  }, [nickname]);
 
   return (
     <section className="settings">
       <input type="color" onChange={onChangeColor} defaultValue={color} />
-      <div>This is new new Modal</div>
+      {possibleToInput ? (
+        <NickNameInput />
+      ) : (
+        <>
+          <span>{nickname}</span>
+          <button onClick={changeNickname}>
+            <i className="bi bi-person-square"></i>
+          </button>
+        </>
+      )}
     </section>
   );
 };
--- my-world/src/components/Welcome/Welcome.tsx
+++ my-world/src/components/Welcome/Welcome.tsx
@@ -1,38 +1,29 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "modules";
+import { NickNameInput } from "./NicknameInput";
 import SPINNER from "assets/img/loading.png";
 import "components/Welcome/welcome.scss";
+
 export const Welcome = () => {
-  const { color } = useSelector((state: RootState) => state.SettingReducer);
-
-  const [nickname, setNickname] = useState<String | null>(null);
-  const [fadeOutEffect, setFadeOutEffect] = useState("");
-  const nameRef = useRef<HTMLInputElement>(null);
-
-  const onSubmitName = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (nameRef.current) {
-      localStorage.setItem("nickname", nameRef.current.value);
-      setNickname(nameRef.current.value);
-    }
-  };
-
-  useEffect(() => {
-    const storedNick = localStorage.getItem("nickname");
-    if (storedNick) setNickname(storedNick);
-  }, []);
+  const { color, nickname } = useSelector(
+    (state: RootState) => state.SettingReducer
+  );
+  const [fadeOutEffect, setFadeOutEffect] = useState<string | null>(null);
 
   useEffect(() => {
     if (nickname === null) return;
 
-    setFadeOutEffect("fadeout");
+    setFadeOutEffect((prev) => {
+      if (prev === null) return "fadeout";
+      else return prev;
+    });
     const timer = setTimeout(() => {
       const welcomeSection = document.getElementById("welcome");
       if (welcomeSection) {
         setFadeOutEffect((prev) => prev + " disappearWelcome");
       }
-    }, 3000);
+    }, 2000);
 
     return () => clearTimeout(timer);
   }, [nickname]);
@@ -53,13 +44,7 @@ export const Welcome = () => {
       ) : (
         <>
           <h2>반갑습니다</h2>
-          <form onSubmit={onSubmitName}>
-            <input
-              type="text"
-              ref={nameRef}
-              placeholder="사용자 이름을 입력해주세요"
-            />
-          </form>
+          <NickNameInput />
         </>
       )}
     </section>
